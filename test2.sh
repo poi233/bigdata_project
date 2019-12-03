25e67a8eed3af681f9d73d81f09893615e264f79
poi233 <593657039@qq.com>
2019-12-03 17:26:43 -0500

    add task2 function & add data for task1 & modify task2 data to sample 50

--- test2.sh
+++ test2.sh
@@ -3,4 +3,4 @@ source ~/.bashrc
 module load python/gnu/3.6.5
 module load spark/2.4.0
 rm -r task2_data/
-spark-submit --conf spark.pyspark.python=/share/apps/python/3.6.5/bin/python task2.py
+spark-submit --conf spark.pyspark.python=/share/apps/python/3.6.5/bin/python task2_get_data.py
